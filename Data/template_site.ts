6bafed990b651f1919021d8c1f948f679d2355cd
Estiak Ahmed <estiakahmed898@gmail.com>
2025-12-12 14:59:10 +0600

    Templete timing Updated

--- Data/template_site.ts
+++ Data/template_site.ts
@@ -1,125 +1,178 @@
 //Data/template_site
 
 export const DEFAULT_SOCIAL_SITES = [
-  { name: "Behance", url: "https://www.behance.net/", isRequired: true },
-  { name: "YouTube", url: "https://www.youtube.com", isRequired: true },
-  { name: "Crunchbase", url: "https://www.crunchbase.com/", isRequired: true },
-  { name: "MuckRack", url: "https://muckrack.com/", isRequired: true },
-  { name: "Issuu", url: "https://issuu.com/", isRequired: true },
-  { name: "About.me", url: "https://about.me/", isRequired: true },
-  { name: "Flipboard", url: "https://flipboard.com/", isRequired: true },
-  { name: "Linktree", url: "https://linktr.ee/", isRequired: true },
-  { name: "Foursquare", url: "https://foursquare.com", isRequired: true },
+  { name: "Behance", url: "https://www.behance.net/", isRequired: true, defaultIdealDurationMinutes: 30 },
 
-  { name: "HubPages", url: "https://hubpages.com", isRequired: true },
-  {
-    name: "Quora Profile",
-    url: "https://www.quora.com/profile",
-    isRequired: true,
-  },
-  { name: "Cake.me", url: "https://www.cake.me/", isRequired: true },
-  { name: "Scoop.it", url: "https://www.scoop.it/u/", isRequired: true },
-  { name: "Gravatar", url: "https://gravatar.com/", isRequired: true },
-  { name: "Giphy Channel", url: "https://giphy.com/channel", isRequired: true },
-  { name: "Completed.com", url: "https://completed.com/", isRequired: true },
-  { name: "JustPaste.it", url: "https://justpaste.it", isRequired: true },
+  { name: "YouTube", url: "https://www.youtube.com", isRequired: true, defaultIdealDurationMinutes: 20 },
 
-  { name: "SlideShare", url: "https://www.slideshare.net", isRequired: true },
-  { name: "Slides.com", url: "https://slides.com", isRequired: true },
-  { name: "Pinterest", url: "https://www.pinterest.com/", isRequired: true },
-  { name: "500px", url: "https://500px.com/", isRequired: true },
-  { name: "SpeakerDeck", url: "https://speakerdeck.com", isRequired: true },
+  { name: "Crunchbase", url: "https://www.crunchbase.com/", isRequired: true, defaultIdealDurationMinutes: 20 },
 
-  { name: "Bluesky", url: "https://bsky.app/profile", isRequired: true },
-  { name: "Substack", url: "https://substack.com", isRequired: true },
-  { name: "SpeakerHub", url: "https://speakerhub.com", isRequired: true },
-  { name: "VelvetJobs", url: "https://www.velvetjobs.com", isRequired: true },
-  { name: "Webflow", url: "https://webflow.io/", isRequired: true },
-  { name: "Reddit", url: "https://reddit.com", isRequired: true },
+  { name: "MuckRack", url: "https://muckrack.com/", isRequired: true, defaultIdealDurationMinutes: 20 },
 
-  { name: "F6S", url: "https://www.f6s.com/", isRequired: true },
-  { name: "Sessionize", url: "https://sessionize.com/", isRequired: true },
-  { name: "Wattpad", url: "https://www.wattpad.com", isRequired: true },
-  { name: "DoYouBuzz", url: "https://www.doyoubuzz.com/us", isRequired: true },
-  { name: "Infogram", url: "https://infogram.com/", isRequired: true },
+  { name: "Issuu", url: "https://issuu.com/", isRequired: true, defaultIdealDurationMinutes: 20 },
 
-  {
-    name: "Creator Spring",
-    url: "https://www.creator-spring.com/",
-    isRequired: true,
-  },
+  { name: "About.me", url: "https://about.me/", isRequired: true, defaultIdealDurationMinutes: 17 },
+
+  { name: "Flipboard", url: "https://flipboard.com/", isRequired: true, defaultIdealDurationMinutes: 15 },
+
+  { name: "Linktree", url: "https://linktr.ee/", isRequired: true, defaultIdealDurationMinutes: 16 },
+
+  { name: "HubPages", url: "https://hubpages.com", isRequired: true, defaultIdealDurationMinutes: 22 },
+
+  { name: "Quora Profile", url: "https://www.quora.com/profile", isRequired: true, defaultIdealDurationMinutes: 25 },
+
+  { name: "Cake.me", url: "https://www.cake.me/", isRequired: true, defaultIdealDurationMinutes: 25 },
+
+  { name: "Scoop.it", url: "https://www.scoop.it/u/", isRequired: true, defaultIdealDurationMinutes: 13 },
+
+  { name: "Gravatar", url: "https://gravatar.com/", isRequired: true, defaultIdealDurationMinutes: 17 },
+
+  { name: "Giphy Channel", url: "https://giphy.com/channel", isRequired: true, defaultIdealDurationMinutes: 16 },
+
+  { name: "Completed.com", url: "https://completed.com/", isRequired: true, defaultIdealDurationMinutes: 16 },
+
+  { name: "JustPaste.it", url: "https://justpaste.it", isRequired: true, defaultIdealDurationMinutes: 13 },
+
+  { name: "SlideShare", url: "https://www.slideshare.net", isRequired: true, defaultIdealDurationMinutes: 25 },
+
+  { name: "Slides.com", url: "https://slides.com", isRequired: true, defaultIdealDurationMinutes: 14 },
+
+  { name: "Pinterest", url: "https://www.pinterest.com/", isRequired: true, defaultIdealDurationMinutes: 25 },
+
+  { name: "500px", url: "https://500px.com/", isRequired: true, defaultIdealDurationMinutes: 20 },
+
+  { name: "SpeakerDeck", url: "https://speakerdeck.com", isRequired: true, defaultIdealDurationMinutes: 16 },
+
+  { name: "Bluesky", url: "https://bsky.app/profile", isRequired: true, defaultIdealDurationMinutes: 12 },
+
+  { name: "Substack", url: "https://substack.com", isRequired: true, defaultIdealDurationMinutes: 20 },
+
+  { name: "SpeakerHub", url: "https://speakerhub.com", isRequired: true, defaultIdealDurationMinutes: 30 },
+
+  { name: "VelvetJobs", url: "https://www.velvetjobs.com", isRequired: true, defaultIdealDurationMinutes: 16 },
+
+  { name: "Webflow", url: "https://webflow.io/", isRequired: true, defaultIdealDurationMinutes: 20 },
+
+  { name: "Reddit", url: "https://reddit.com", isRequired: true, defaultIdealDurationMinutes: 15 },
+
+  { name: "F6S", url: "https://www.f6s.com/", isRequired: true, defaultIdealDurationMinutes: 18 },
+
+  { name: "Sessionize", url: "https://sessionize.com/", isRequired: true, defaultIdealDurationMinutes: 14 },
+
+  { name: "Wattpad", url: "https://www.wattpad.com", isRequired: true, defaultIdealDurationMinutes: 20 },
+
+  { name: "DoYouBuzz", url: "https://www.doyoubuzz.com/us", isRequired: true, defaultIdealDurationMinutes: 18 },
+
+  { name: "Infogram", url: "https://infogram.com/", isRequired: true, defaultIdealDurationMinutes: 18 },
+
+  { name: "Creator Spring", url: "https://www.creator-spring.com/", isRequired: true, defaultIdealDurationMinutes: 20 },
+
+  { name: "BlogLovin", url: "https://www.bloglovin.com", isRequired: true, defaultIdealDurationMinutes: 14 },
 ];
 
 export const DEFAULT_WEB2_SITES = [
-  { name: "Medium", url: "https://medium.com/", isRequired: true },
-  { name: "Strikingly", url: "https://www.strikingly.com/", isRequired: true },
-  { name: "Tumblr", url: "https://www.tumblr.com/", isRequired: true },
-  { name: "WordPress", url: "https://wordpress.com/", isRequired: true },
+  { name: "Medium", url: "https://medium.com/", isRequired: true, defaultIdealDurationMinutes: 30 },
 
-  { name: "Blogger", url: "https://www.blogger.com/", isRequired: true },
-  { name: "Jimdo", url: "https://jimdo.com", isRequired: true },
+  { name: "Strikingly", url: "https://www.strikingly.com/", isRequired: true, defaultIdealDurationMinutes: 40 },
 
-  {
-    name: "Bravenet",
-    url: "https://www.bravenet.com/register",
-    isRequired: true,
-  },
-  { name: "Weebly", url: "https://weebly.com", isRequired: true },
+  { name: "Tumblr", url: "https://www.tumblr.com/", isRequired: true, defaultIdealDurationMinutes: 25 },
 
-  {
-    name: "Google Sites",
-    url: "https://sites.google.com/new",
-    isRequired: true,
-  },
+  { name: "WordPress", url: "https://wordpress.com/", isRequired: true, defaultIdealDurationMinutes: 40 },
 
-  { name: "JotForm", url: "http://form.jotform.me/", isRequired: true },
+  { name: "Blogger", url: "https://www.blogger.com/", isRequired: true, defaultIdealDurationMinutes: 30 },
 
-  {
-    name: "Vocal Media (Authors)",
-    url: "https://vocal.media/authors",
-    isRequired: true,
-  },
+  { name: "Jimdo", url: "https://jimdo.com", isRequired: true, defaultIdealDurationMinutes: 35 },
+
+  { name: "Site123.me", url: "https://site123.me", isRequired: true, defaultIdealDurationMinutes: 40 },
+
+  { name: "Weebly", url: "https://weebly.com", isRequired: true, defaultIdealDurationMinutes: 30 },
+
+  { name: "Google Sites", url: "https://sites.google.com/new", isRequired: true, defaultIdealDurationMinutes: 30 },
+
+  { name: "JotForm", url: "http://form.jotform.me/", isRequired: true, defaultIdealDurationMinutes: 30 },
+
+  { name: "Vocal Media (Authors)", url: "https://vocal.media/authors", isRequired: true, defaultIdealDurationMinutes: 20 },
+
+  { name: "Wix", url: "https://wix.com", isRequired: true, defaultIdealDurationMinutes: 40 },
 ];
 
+
 export const DEFAULT_ADDITIONAL_SITES = [
-  { name: "Triberr", url: "https://triberr.com/", isRequired: true },
-
-  { name: "OpenStreetMap", url: "https://openstreetmap.org", isRequired: true },
-
-  { name: "Disqus", url: "https://disqus.com", isRequired: true },
-
-  { name: "Dwell", url: "https://www.dwell.com/", isRequired: true },
-
+  {
+    name: "Triberr",
+    url: "https://triberr.com/",
+    isRequired: true,
+    defaultIdealDurationMinutes: 20,
+  },
+  {
+    name: "Disqus",
+    url: "https://disqus.com",
+    isRequired: true,
+    defaultIdealDurationMinutes: 15,
+  },
+  {
+    name: "Dwell",
+    url: "https://www.dwell.com/",
+    isRequired: true,
+    defaultIdealDurationMinutes: 15,
+  },
   {
     name: "Creator Spring",
     url: "https://www.creator-spring.com/",
     isRequired: true,
+    defaultIdealDurationMinutes: 20,
   },
-
   {
     name: "Product Hunt",
     url: "https://www.producthunt.com",
     isRequired: true,
+    defaultIdealDurationMinutes: 15,
   },
-
-  { name: "Docker Hub", url: "https://hub.docker.com", isRequired: true },
-
-  { name: "Xing", url: "https://www.xing.com/en", isRequired: true },
-
-  { name: "Last.fm", url: "https://www.last.fm/", isRequired: true },
-
+  {
+    name: "Docker Hub",
+    url: "https://hub.docker.com",
+    isRequired: true,
+    defaultIdealDurationMinutes: 15,
+  },
+  {
+    name: "Xing",
+    url: "https://www.xing.com/en",
+    isRequired: true,
+    defaultIdealDurationMinutes: 20,
+  },
+  {
+    name: "Last.fm",
+    url: "https://www.last.fm/",
+    isRequired: true,
+    defaultIdealDurationMinutes: 15,
+  },
   {
     name: "Wellfound (AngelList Talent)",
     url: "https://wellfound.com/",
     isRequired: true,
+    defaultIdealDurationMinutes: 15,
+  },
+  {
+    name: "Padlet",
+    url: "https://padlet.com/",
+    isRequired: true,
+    defaultIdealDurationMinutes: 20,
+  },
+  {
+    name: "Diigo",
+    url: "https://diigo.com",
+    isRequired: true,
+    defaultIdealDurationMinutes: 15,
   },
 ];
+
 
 export const DEFAULT_GRAPHICS_DESIGN = [
   {
     name: "Graphic Design",
     url: "https://drive.google.com/drive/home",
     isRequired: true,
+    defaultIdealDurationMinutes: 90,
   },
 ];
 
@@ -128,6 +181,7 @@ export const DEFAULT_IMAGE_OPTIMIZATION = [
     name: "Image Optimization",
     url: "https://drive.google.com/drive/home",
     isRequired: true,
+    defaultIdealDurationMinutes: 120,
   },
 ];
 
@@ -136,6 +190,7 @@ export const DEFAULT_CONTENT_STUDIO = [
     name: "Content Studio",
     url: "https://contentstudio.io/",
     isRequired: true,
+    defaultIdealDurationMinutes: 80,
   },
 ];
 
@@ -144,11 +199,13 @@ export const DEFAULT_CONTENT_WRITING = [
     name: "Content Writing",
     url: "https://docs.google.com/document/u/0/",
     isRequired: true,
+    defaultIdealDurationMinutes: 30,
   },
 ];
 
 export const DEFAULT_BACKLINKS = [
-  { name: "Backlinks", url: "https://backlinko.com/", isRequired: true },
+  { name: "Backlinks", url: "https://backlinko.com/", isRequired: true, defaultIdealDurationMinutes: 30 },
+
 ];
 
 export const DEFAULT_YOUTUBE_VIDEO_OPTIMIZATION = [
@@ -156,6 +213,7 @@ export const DEFAULT_YOUTUBE_VIDEO_OPTIMIZATION = [
     name: "YouTube Video Optimization",
     url: "https://www.youtube.com/",
     isRequired: true,
+    defaultIdealDurationMinutes: 60
   },
 ];
 
